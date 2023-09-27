c506ef930799441b4ba2aae4dbfe174dd408baba
pujith25 <116423457+pujith25@users.noreply.github.com>
2023-09-27 13:19:44 +0530

    commit changes

--- index.js
+++ index.js
@@ -12,237 +12,185 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
-
+let cardImg = document.getElementById("card-img")
 playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
     let randomColor = Math.floor( Math.random()*4 ) + 1
-    const img = new Image(120, 200);
+   // const img = new Image(120, 200);
+   let str = ""
+   let ans = -1
     if(randomColor === 1){
        if (randomNumber === 1 ) {
-           img.src = "images/AC.png"
-           document.getElementById("card-img").appendChild(img)
-           return 11
+           str = "<img id = 'image' src = 'images/AC.png'>"
+           ans = 11
        } else if (randomNumber === 2) {
-           img.src = "images/2C.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
+           str = "<img id = 'image' src = 'images/2C.png'>"
+           ans = randomNumber
        } else if (randomNumber === 3) {
-           img.src = "images/3C.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
+           str = "<img id = 'image' src = 'images/3C.png'>"
+           ans = randomNumber
        } else if (randomNumber === 4) {
-           img.src = "images/4C.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
+        str = "<img id = 'image' src = 'images/4C.png'>"
+        ans = randomNumber
        } else if (randomNumber === 5) {
-           img.src = "images/5C.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
+        str = "<img id = 'image' src = 'images/5C.png'>"
+        ans = randomNumber
        } else if (randomNumber === 6) {
-           img.src = "images/6C.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
+        str = "<img id = 'image' src = 'images/6C.png'>"
+        ans = randomNumber
        } else if (randomNumber === 7) {
-           img.src = "images/7C.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
+        str = "<img id = 'image' src = 'images/7C.png'>"
+        ans = randomNumber
        } else if (randomNumber === 8) {
-           img.src = "images/8C.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
+        str = "<img id = 'image' src = 'images/8C.png'>"
+        ans = randomNumber
        } else if (randomNumber === 9) {
-           img.src = "images/9C.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
+        str = "<img id = 'image' src = 'images/9C.png'>"
+        ans = randomNumber
        } else if (randomNumber === 10) {
-           img.src = "images/10C.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
+        str = "<img id = 'image' src = 'images/10C.png'>"
+        ans = randomNumber
        } else if (randomNumber === 11) {
-           img.src = "images/JC.png"
-           document.getElementById("card-img").appendChild(img)
-           return 10
+        str = "<img id = 'image' src = 'images/JC.png'>"
+        ans = 10
        } else if (randomNumber === 12) {
-           img.src = "images/QC.png"
-           document.getElementById("card-img").appendChild(img)
-           return 10
+        str = "<img id = 'image' src = 'images/QC.png'>"
+        ans = 10
        } else if (randomNumber === 13) {
-           img.src = "images/KC.png"
-           document.getElementById("card-img").appendChild(img)
-           return 10
+        str = "<img id = 'image' src = 'images/KC.png'>"
+        ans = 10
        }
     }
     else if(randomColor === 2){
-       if (randomNumber === 1 ) {
-           img.src = "images/AD.png"
-           document.getElementById("card-img").appendChild(img)
-           return 11
-       } else if (randomNumber === 2) {
-           img.src = "images/2D.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 3) {
-           img.src = "images/3D.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 4) {
-           img.src = "images/4D.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 5) {
-           img.src = "images/5D.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 6) {
-           img.src = "images/6D.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 7) {
-           img.src = "images/7D.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 8) {
-           img.src = "images/8D.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 9) {
-           img.src = "images/9D.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 10) {
-           img.src = "images/10D.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 11) {
-           img.src = "images/JD.png"
-           document.getElementById("card-img").appendChild(img)
-           return 10
-       } else if (randomNumber === 12) {
-           img.src = "images/QD.png"
-           document.getElementById("card-img").appendChild(img)
-           return 10
-       } else if (randomNumber === 13) {
-           img.src = "images/KD.png"
-           document.getElementById("card-img").appendChild(img)
-           return 10
-       }
-       
+        if (randomNumber === 1 ) {
+            str = "<img id = 'image' src = 'images/AD.png'>"
+            ans = 11
+        } else if (randomNumber === 2) {
+            str = "<img id = 'image' src = 'images/2D.png'>"
+            ans = randomNumber
+        } else if (randomNumber === 3) {
+            str = "<img id = 'image'src = 'images/3D.png'>"
+            ans = randomNumber
+        } else if (randomNumber === 4) {
+         str = "<img id = 'image' src = 'images/4D.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 5) {
+         str = "<img id = 'image' src = 'images/5D.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 6) {
+         str = "<img id = 'image' src = 'images/6D.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 7) {
+         str = "<img id = 'image' src = 'images/7D.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 8) {
+         str = "<img id = 'image' src = 'images/8D.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 9) {
+         str = "<img id = 'image' src = 'images/9D.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 10) {
+         str = "<img id = 'image' src = 'images/10D.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 11) {
+         str = "<img id = 'image' src = 'images/JD.png'>"
+         ans = 10
+        } else if (randomNumber === 12) {
+         str = "<img id = 'image' src = 'images/QD.png'>"
+         ans = 10
+        } else if (randomNumber === 13) {
+         str = "<img id = 'image' src = 'images/KD.png'>"
+         ans = 10
+        }
     }
     else if(randomColor === 3){
-       if (randomNumber === 1 ) {
-           img.src = "images/AH.png"
-           document.getElementById("card-img").appendChild(img)
-           return 11
-       } else if (randomNumber === 2) {
-           img.src = "images/2H.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 3) {
-           img.src = "images/3H.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 4) {
-           img.src = "images/4H.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 5) {
-           img.src = "images/5H.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 6) {
-           img.src = "images/6H.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 7) {
-           img.src = "images/7H.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 8) {
-           img.src = "images/8H.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 9) {
-           img.src = "images/9H.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 10) {
-           img.src = "images/10H.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 11) {
-           img.src = "images/JH.png"
-           document.getElementById("card-img").appendChild(img)
-           return 10
-       } else if (randomNumber === 12) {
-           img.src = "images/QH.png"
-           document.getElementById("card-img").appendChild(img)
-           return 10
-       } else if (randomNumber === 13) {
-           img.src = "images/KH.png"
-           document.getElementById("card-img").appendChild(img)
-           return 10
-       }
-       
+        if (randomNumber === 1 ) {
+            str = "<img id = 'image' src = 'images/AH.png'>"
+            ans = 11
+        } else if (randomNumber === 2) {
+            str = "<img id = 'image' src = 'images/2H.png'>"
+            ans = randomNumber
+        } else if (randomNumber === 3) {
+            str = "<img id = 'image' src = 'images/3H.png'>"
+            ans = randomNumber
+        } else if (randomNumber === 4) {
+         str = "<img id = 'image' src = 'images/4H.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 5) {
+         str = "<img id = 'image' src = 'images/5H.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 6) {
+         str = "<img id = 'image' src = 'images/6H.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 7) {
+         str = "<img id = 'image' src = 'images/7H.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 8) {
+         str = "<img id = 'image' src = 'images/8H.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 9) {
+         str = "<img id = 'image' src = 'images/9H.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 10) {
+         str = "<img id = 'image' src = 'images/10H.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 11) {
+         str = "<img id = 'image' src = 'images/JH.png'>"
+         ans = 10
+        } else if (randomNumber === 12) {
+         str = "<img id = 'image'  src = 'images/QH.png'>"
+         ans = 10
+        } else if (randomNumber === 13) {
+         str = "<img id = 'image' src = 'images/KH.png'>"
+         ans = 10
+        } 
     }
     else if(randomColor === 4){
-       if (randomNumber === 1 ) {
-           img.src = "images/AS.png"
-           document.getElementById("card-img").appendChild(img)
-           return 11
-       } else if (randomNumber === 2) {
-           img.src = "images/2S.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 3) {
-           img.src = "images/3S.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 4) {
-           img.src = "images/4S.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 5) {
-           img.src = "images/5S.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 6) {
-           img.src = "images/6S.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 7) {
-           img.src = "images/7S.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 8) {
-           img.src = "images/8S.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 9) {
-           img.src = "images/9S.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 10) {
-           img.src = "images/10S.png"
-           document.getElementById("card-img").appendChild(img)
-           return randomNumber
-       } else if (randomNumber === 11) {
-           img.src = "images/JS.png"
-           document.getElementById("card-img").appendChild(img)
-           return 10
-       } else if (randomNumber === 12) {
-           img.src = "images/QS.png"
-           document.getElementById("card-img").appendChild(img)
-           return 10
-       } else if (randomNumber === 13) {
-           img.src = "images/KS.png"
-           document.getElementById("card-img").appendChild(img)
-           return 10
-       }
-       
+        if (randomNumber === 1 ) {
+            str = "<img id = 'image' src = 'images/AS.png'>"
+            ans = 11
+        } else if (randomNumber === 2) {
+            str = "<img id = 'image' src = 'images/2S.png'>"
+            ans = randomNumber
+        } else if (randomNumber === 3) {
+            str = "<img id = 'image' src = 'images/3S.png'>"
+            ans = randomNumber
+        } else if (randomNumber === 4) {
+         str = "<img id = 'image' src = 'images/4S.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 5) {
+         str = "<img id = 'image' src = 'images/5S.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 6) {
+         str = "<img id = 'image' src = 'images/6S.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 7) {
+         str = "<img id = 'image' src = 'images/7S.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 8) {
+         str = "<img id = 'image' src = 'images/8S.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 9) {
+         str = "<img id = 'image' src = 'images/9S.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 10) {
+         str = "<img id = 'image' src = 'images/10S.png'>"
+         ans = randomNumber
+        } else if (randomNumber === 11) {
+         str = "<img id = 'image' src = 'images/JS.png'>"
+         ans = 10
+        } else if (randomNumber === 12) {
+         str = "<img id = 'image' src = 'images/QS.png'>"
+         ans = 10
+        } else if (randomNumber === 13) {
+         str = "<img id = 'image' src = 'images/KS.png'>"
+         ans = 10
+        }   
     }
-    
+ cardImg.innerHTML += str 
+ return ans
 }
 
 function removeImageById(imageId) {
